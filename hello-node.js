2a7538c596cf1c18e5d27585bf911816bcca6a79
Sanders <jsanders8@una.edu>
2022-08-23 13:39:42 -0500

    added node and express

---
+++ hello-node.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+console.log("hello from node");
+
+app.get('/', function (req, res) {
+  res.send('Hello World from Express')
+})
+
+app.listen(3001)
+
+
